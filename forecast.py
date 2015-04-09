6e1f680a7f6686ec346429298698f366019cc836
zeferino <mpe1990@msn.com>
2015-04-09 23:52:15 +0200

    Electrovalve: - humiditySensor: - Levelsensor: - temperatureSensor: - gps: implementadas las dos funciones ardu_riego: ardu_riego_v2: forecast: new function

--- forecast.py
+++ forecast.py
@@ -5,10 +5,13 @@ import requests
 class forecast:
     def __init__(self):
         self.apiId = '0322a8ae89b2d00a65ef04c33ac4a37d'
-        self.coords = {'lat': "0", 'lon': "0"}
+        self.params = {'lat': "0", 'lon': "0", 'APPID': self.apiId}
         self.urlForecast = "http://api.openweathermap.org/data/2.5/weather"
         self.urlHistorical = "http://api.openweathermap.org/data/2.5/history/city"
         self.forecastJson = " "
         self.historicalJson = " "
 
-    def
+    def getCurrentWeather(self):
+        r = requests.get(self.urlForecast, params=self.params)
+        self.forecastJson = r.json()
+        return self.forecastJson
