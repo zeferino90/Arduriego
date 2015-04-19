a5ce5824b44b2b393ab5f047db640d0ee4727cd1
zeferino90 <mperesp1990@gmail.com>
2015-04-19 19:59:33 +0200

    setup y plant añadidos

--- humiditySensor.py
+++ humiditySensor.py
@@ -8,7 +8,7 @@ class humiditySensor:
         self.arduino = serial.Serial('/dev/ttyACM0', 115200, timeout= 1.0)
 
     def getvalue(self, addr):
-        if addr < 1 and addr > 3:
+        if addr < 1 or addr > 3:
             self.quality = self.quality if self.quality != 1 else 0
         else:
             self.arduino.write("H"+ str(addr))
