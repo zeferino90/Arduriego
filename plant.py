3617854863ef43a32ff4b87b13adb4c9d58b4c3e
zeferino90 <mperesp1990@gmail.com>
2015-05-10 15:36:25 +0200

    buf fixes

--- plant.py
+++ plant.py
@@ -43,8 +43,8 @@ class plant:
     def getPostpone(self):
         return self.postpone
 
-     def getHumidity(self):
-         return humiditySensor().getvalue(self.humidityaddres)
+    def getHumidity(self):
+        return humiditySensor().getvalue(self.humidityaddres)
 
     def setName(self, newname):
         self.name = newname
@@ -61,8 +61,8 @@ class plant:
     def setPostpone(self, newPostpone):
         self.postpone = newPostpone
 
-     def watered(self):
-         self.lastw = datetime.today()
+    def watered(self):
+        self.lastw = datetime.today()
 
     def nextWateringTime(self):
         return self.lastw + self.cycle
