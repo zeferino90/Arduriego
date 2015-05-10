553a7abfe87473620a7e349112d45f1c084c4321
zeferino90 <mperesp1990@gmail.com>
2015-05-10 22:15:57 +0200

    serial para debuguear arduino

--- serial_prueva/serial_raspduino.py
+++ serial_prueva/serial_raspduino.py
@@ -1,15 +1,21 @@
 import serial
  
-arduino = serial.Serial('/dev/ttyACM0', 9600)
+arduino = serial.Serial('/dev/ttyACM0', 115200)
  
 print("Starting!")
  
 while True:
       comando = raw_input('Introduce un comando: ') #Input
       arduino.write(comando) #Mandar un comando hacia Arduino
-      if comando == 'H':
-            print('LED ENCENDIDO')
-      elif comando == 'L':
-            print('LED APAGADO')
+      s = arduino.readline()
+      print s
+      print("\n")
+      s = arduino.readline()
+      print s
+      print("\n")
+      #if comando == 'H':
+      #      print('LED ENCENDIDO')
+      #elif comando == 'L':
+      #      print('LED APAGADO')
  
 arduino.close() #Finalizamos la comunicacion
